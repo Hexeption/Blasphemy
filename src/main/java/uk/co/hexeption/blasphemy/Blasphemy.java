e8c34ad87dca14f59dcbbf6c1a009d5b12812022
Hexeption <minecraftfun201@gmail.com>
2020-03-28 00:09:35 +0000

    Added Mod

---
+++ src/main/java/uk/co/hexeption/blasphemy/Blasphemy.java
@@ -0,0 +1,24 @@
+package uk.co.hexeption.blasphemy;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.tag.FabricItemTags;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.Settings;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Blasphemy implements ModInitializer {
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+        new Identifier("Blasphemy", "general"),
+        () -> new ItemStack(Blocks.COBBLESTONE));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("blasphemy", "wand"), new Item(new Settings().group(ITEM_GROUP)));
+    }
+}
